760903521f543df1dea713f4dff54349b085a120
Emmanuel Sanchez <128549074+WorriorBeast@users.noreply.github.com>
2025-01-21 19:01:50 -0800

    Build a balanced binary tree from an array

--- src/index.js
+++ src/index.js
@@ -10,7 +10,26 @@ class Node {
 
 class Tree {
 	constructor(array) {
-		this.array = array;
-		this.root = null;
+		this.root = this.buildTree(array);
+	}
+
+	buildTree(array) {
+		if (array.length === 0) return null;
+
+		const uniqueArray = [...new Set(array)].sort((a, b) => {
+			return a - b;
+		});
+		const middle = Math.floor(uniqueArray.length / 2);
+		const root = new Node(uniqueArray[middle]);
+
+		root.left = this.buildTree(uniqueArray.slice(0, middle));
+		root.right = this.buildTree(uniqueArray.slice(middle + 1));
+
+		return root;
 	}
 }
+
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const tree = new Tree(array);
+
+console.log(tree.root);
