a2062b232dd9db7f9c5d1841e12ad23d095d646f
Emmanuel Sanchez <128549074+WorriorBeast@users.noreply.github.com>
2025-01-22 18:39:55 -0800

    Insert new node into balanced binary search tree

--- src/index.js
+++ src/index.js
@@ -27,9 +27,42 @@ class Tree {
 
 		return root;
 	}
+
+	insert(value) {
+		let parent;
+		let current = this.root;
+
+		while (current) {
+			parent = current;
+
+			if (current.value > value) {
+				current = current.left;
+			} else if (current.value < value) {
+				current = current.right;
+			}
+		}
+
+		if (parent.value === value) return;
+
+		const node = new Node(value);
+
+		if (parent.value > value) {
+			parent.left = node;
+		} else {
+			parent.right = node;
+		}
+
+		return this.root;
+	}
 }
 
 const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const tree = new Tree(array);
+
+console.log(tree.root);
+
+tree.insert(10);
+tree.insert(12);
+tree.insert(11);
 
 console.log(tree.root);
